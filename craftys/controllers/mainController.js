ce1ca6ed5343dfb231f97dc3c75560ac7618b524
Marcos Urdinola <marcosurdinola07@gmail.com>
2021-08-31 12:20:54 -0300

    Borre carpeta craftsy y wireframes

--- craftys/controllers/mainController.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-
-module.exports = 
-    index:(req,res => res.sendFile (path.join(--dirname ,"views", "index",))
-admin:(req,res) => res.sendFile (path.join(--dirname "","views", "index",))
