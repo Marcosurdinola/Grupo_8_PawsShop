8fe8fb0fd2fa320932993b81c0a9244b18a7be5d
Marcos Urdinola <marcosurdinola07@gmail.com>
2021-09-10 11:11:27 -0300

    maquetado expresgenerator y ruteo done

---
+++ sitio/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const {index, carrito} = require('../controllers/mainController')
+
+/* GET home page. */
+router.get('/', index);
+router.get('/carrito', carrito)
+
+module.exports = router;
